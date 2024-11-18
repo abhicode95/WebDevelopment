5b9da9c476b1893948b9171a3c5f117e60fc7f91
abhicode95 <abhiharman2103@gmail.com>
2024-11-18 14:51:37 +0530

    Tailwind css changes

--- TailwindCss/tailwind.config.js
+++ TailwindCss/tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        my_teal: "#008080",
+        pale_orange: "#ffd9a6",
+        light_orange: "#fbb03b",
+      },
+      fontFamily: {
+        sans: ["Bebas Neue", "sans-serif"],
+        slab: ["Roboto Slab", "serif"],
+      },
+    },
   },
   plugins: [],
 };
